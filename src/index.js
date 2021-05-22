94f7414879b5c317d9d8f8a0c9ea0e24e53dbfa3
Kathy Rodriguez <katrrodriguez26@gmail.com>
2021-05-21 23:13:30 -0400

    Added service workers

--- src/index.js
+++ src/index.js
@@ -7,5 +7,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+import * as serviceWorker from './serviceWorker';
 
 
+ReactDOM.render(<App/>, document.getElementById("root"));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
