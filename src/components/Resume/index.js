cebece838502e2f6407779cfbbb23090e6bcef3e
Kathy Rodriguez <katrrodriguez26@gmail.com>
2021-05-25 16:34:24 -0400

    edited resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,7 +7,7 @@ function Resume() {
 				<h1 className="page-header">My Resume</h1>
 			</div>
 			<div className="bottom-spacing">
-				<a href={require("../../assets/files/Kathleen Rodrigue.pdf")} download>
+				<a href={require("../../assets/files/Kathleen Rodriguez.pdf")} download>
 					<h4>Download my Resume</h4>
 				</a>
 			</div>
@@ -20,8 +20,6 @@ function Resume() {
 					<li>jQuery</li>
 					<li>Bootstrap</li>
 					<li>React</li>
-					<li>version control/Git</li> 
-					<li>responsive design</li>
 				</ol>
 				<br></br>
 				<h5>Back-End Proficiencies</h5>
@@ -30,15 +28,12 @@ function Resume() {
 					<li>NodeJS</li>
 					<li>Express</li>
 					<li>Model View Controller (MVC)</li>
-					<li>REST</li>
-					<li>Progressive Web Applications (PWA)</li>
 				</ol>
 				<br></br>
 				<h5>Dev Tool Proficiencies</h5>
 				<ol>
 					<li>Git</li>
 					<li>npm</li>
-					<li>Jest</li>
 					<li>Webpack</li>
 				</ol>
 				<br></br>
@@ -47,8 +42,6 @@ function Resume() {
 					<li>MySQL</li>
 					<li>Sequelize</li>
 					<li>NoSQL</li>
-					<li>SQLite</li>
-					<li>GraphQL</li>
 					<li>MongoDB</li>
 					<li>Mongoose</li>
 				</ol>
